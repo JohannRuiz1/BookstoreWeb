27619d1751c7cb10b0ed16e88634017b83baabbc
Johann Ruiz <johannruiz176@vt.edu>
2024-07-07 00:32:51 -0400

    Added cart table and animation on dropdown

--- JohannBookstoreSession-Client/src/components/HeaderDropdown.tsx
+++ JohannBookstoreSession-Client/src/components/HeaderDropdown.tsx
@@ -1,16 +1,24 @@
+import { useState } from "react";
 import CategoryNavLinks from "./CategoryNavLinks.tsx";
 import "./HeaderDropdown.css";
 
 export default function HeaderDropdown() {
+  const [isDropdownVisible, setIsDropdownVisible] = useState(false);
 
   return (
-    <div className="header-dropdown">
+    <div
+      className="header-dropdown"
+      onMouseEnter={() => setIsDropdownVisible(true)}
+      onMouseLeave={() => setIsDropdownVisible(false)}
+    >
       <div className="category">
         <h4 className="category-title">Categories</h4>
         <span className="category-icon">
-            <i className="fa-solid fa-angle-down"></i>
+          <i className="fa-solid fa-angle-down"></i>
         </span>
-        <CategoryNavLinks/>
+        <ul className={`dropdown-menu ${isDropdownVisible ? 'visible' : ''}`}>
+          <CategoryNavLinks />
+        </ul>
       </div>
     </div>
   );
--- JohannBookstoreSession-Client/src/pages/CartPage.tsx
+++ JohannBookstoreSession-Client/src/pages/CartPage.tsx
@@ -1,12 +1,40 @@
+import { Link } from "react-router-dom";
 import CartTable from "../components/CartTable";
 import './CartPage.css'
+import { useCart } from "../contexts/CartContext";
+
 export default function CartPage() {
-return (
+  const { cart, dispatch } = useCart();
+  const clearCart = () => {
+    dispatch({ type: 'CLEAR_CART' });
+  };
+
+  return (
     <div className="cart-page">
-        <h1 className="cart-title">Basket Full of Books!</h1>
-        <CartTable />
+      <h1 className="cart-title">Basket Full of Books!</h1>
+      {cart.numberOfItems > 0 ? (
+        <>
+          <CartTable />
+          <div className="cart-buttons">
+            <Link to="/category/classics" className="no-underline">
+              <button className="cart-button">Continue Shopping?</button>
+            </Link>
+            <button className="cart-button" onClick={clearCart}>Clear Cart</button>
+            <Link to="/checkout" className="no-underline">
+              <button className="cart-button">Proceed to Checkout</button>
+            </Link>
+          </div>
+        </>
+      ) : (
+        <div className="empty-cart-message">
+          <p>Your cart is empty.</p>
+          <div className="cart-buttons">
+            <Link to="/category/classics" className="no-underline">
+              <button className="cart-button">Continue Shopping?</button>
+            </Link>
+          </div>
+        </div>
+      )}
     </div>
-
- );
-
+  );
 }
